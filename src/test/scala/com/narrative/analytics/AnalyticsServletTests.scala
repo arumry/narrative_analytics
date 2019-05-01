7778c40043965f6ea8c62758c34557540031aabb
Aaron Rumery <aaronrumery@gmail.com>
2019-05-01 18:22:15 -0500

    Update tests for new API changes

--- src/test/scala/com/narrative/analytics/AnalyticsServletTests.scala
+++ src/test/scala/com/narrative/analytics/AnalyticsServletTests.scala
@@ -20,17 +20,27 @@ class AnalyticsServletTests extends FunSuite with ScalatraSuite with DBConnectio
       status should equal (200)
     }
 
-    get("/analytics?timestamp=1555987406935", params = None) {
+    get("/analytics?timestamp=1556752792", params = None) {
       status should equal (200)
       body should equal("unique_users,0\nclicks,0\nimpressions,0")
     }
 
-    post(uri = "/analytics?timestamp=1555987406935&user=Aaron&event=Impression", params = None) {
+    get("/analytics?timestamp=", params = None) {
+      status should equal (400)
+      body should equal("You must provide a valid timestamp")
+    }
+
+    post(uri = "/analytics?timestamp=1556752792&user=Aaron&event=Impression", params = None) {
+      status should equal(400)
+      body should equal("You must provide a valid event: click | impression")
+    }
+
+    post(uri = "/analytics?timestamp=1556752792&user=Aaron&event=impression", params = None) {
       status should equal(204)
       body should equal("")
     }
 
-    get("/analytics?timestamp=1555987406935", params = None) {
+    get("/analytics?timestamp=1556752792", params = None) {
       body should equal("unique_users,1\nclicks,0\nimpressions,1")
     }
   }
